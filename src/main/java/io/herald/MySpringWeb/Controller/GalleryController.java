05da80a3bb8a6fcf055a1dc99ee3c167809f8633
NatsuRohit <rohitprajapati2077@gmail.com>
2026-07-27 11:42:11 +0545

    July 27 2026 - feat: started image processing

--- src/main/java/io/herald/MySpringWeb/Controller/GalleryController.java
+++ src/main/java/io/herald/MySpringWeb/Controller/GalleryController.java
@@ -5,6 +5,12 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Base64;
 
 @Controller
 public class GalleryController {
@@ -14,6 +20,21 @@ public class GalleryController {
         if (session.getAttribute("username")==null){
             m.addAttribute("message", "You are not logged in!");
             return "login";
+        }
+        return "galleryPage";
+    }
+
+    @PostMapping("/gallery")
+    public String galleryPost(@RequestParam("image")MultipartFile image){
+        try {
+            byte[] imgBytes = image.getBytes();
+            // We will use Base64 Encoder,
+            // We will encode the byte information of file into string
+            // To decode, we will again use the Base64 Decoder
+
+            String imgString = Base64.getEncoder().encodeToString(imgBytes);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return "galleryPage";
     }

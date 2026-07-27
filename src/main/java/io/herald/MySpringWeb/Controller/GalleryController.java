4653e07219a38eb4c1ba786848631158152e32c0
NatsuRohit <rohitprajapati2077@gmail.com>
2026-07-27 11:04:39 +0545

    July 27 2026 - feat(auth): add session check for gallery and mail page controllers

--- src/main/java/io/herald/MySpringWeb/Controller/GalleryController.java
+++ src/main/java/io/herald/MySpringWeb/Controller/GalleryController.java
@@ -1,12 +1,20 @@
 package io.herald.MySpringWeb.Controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GalleryController {
     @GetMapping("/gallery")
-    public String galleryGet(){
+    public String galleryGet(HttpServletRequest req, Model m){
+        HttpSession session = req.getSession();
+        if (session.getAttribute("username")==null){
+            m.addAttribute("message", "You are not logged in!");
+            return "login";
+        }
         return "galleryPage";
     }
 }
--- src/main/java/io/herald/MySpringWeb/Controller/MailController.java
+++ src/main/java/io/herald/MySpringWeb/Controller/MailController.java
@@ -3,14 +3,16 @@ package io.herald.MySpringWeb.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MailController {
     @GetMapping("/mail")
-    public String mailGet(HttpServletRequest request){
+    public String mailGet(HttpServletRequest request, Model m){
         HttpSession session = request.getSession();
-        if (request.getAttribute("username") == null){
+        if (session.getAttribute("username") == null){
+            m.addAttribute("message","You are not logged in!");
             return "login";
         }
         return "mailPage";
